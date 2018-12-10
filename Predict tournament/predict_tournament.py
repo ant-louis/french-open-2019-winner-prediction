0b3e6b6c8da6609408ac2f8a66fa28b5a0de3ef2
Lambo97 <lamborellemaxime@MacBookMaxime.local>
2018-12-10 09:43:37 +0100

    Clean tournament predicator

--- Predict tournament/predict_tournament.py
+++ Predict tournament/predict_tournament.py
@@ -1,48 +1,48 @@
 import numpy as np
 import pandas as pd
 import random
+from generate_draws import Draws
 
-pred = {}
+class TournamentPredictor:
 
-def gagner(tab):
-    if(len(tab) == 1):
-        return tab[0]
+    def __init__(self):
+        self.pred = {}
+        file_predict = np.array(pd.read_csv("matches.csv", header = 0))
+        players1 = file_predict[:,0]
+        players2 = file_predict[:,1]
+        match_result = file_predict[:,2]
+        for i in range(len(players1)):
+            self.pred[(players1[i],players2[i])] = match_result[i]
+
+    def gagner(self, draw):
+        if(len(draw) == 1):
+            return draw[0]
+        q = int(len(draw)/2)
+        draw1 = draw[0:q]
+        draw2 = draw[q:]
+        return self.predict(self.gagner(draw1), self.gagner(draw2))
+
+    def predict(self, a, b):
+        if(a<b):
+            return self.pred[(a,b)]
+        else:
+            return self.pred[(b,a)]
     
-    q = int(len(tab)/2)
-    tab1 = tab[0:q]
-    tab2 = tab[q:]
-    return predict(gagner(tab1), gagner(tab2))
+    def predict(self):
+        nb_iter = 10000
+        results = np.zeros(len(self.pred))
 
-def predict(a, b):
-    if(a<b):
-        return pred[(a,b)]
-    else:
-        return pred[(b,a)]
+        draw_generate = Draws()
+        draws = draw_generate.generate_draws(nb_iter)
 
-def load(filename):
-    return pd.read_csv(filename, header = 0)
+        for draw in draws:
+            results[self.gagner(draw)-1] += 1
+        results /= results.sum()
+        return results
 
 if __name__ == '__main__':
-    nb_iter = 10000
-
-    tableau = np.array(load("matches.csv"))
-    
-    players1 = tableau[:,0]
-    players2 = tableau[:,1]
-    match_result = tableau[:,2]
-
-    results = np.zeros(len(players1))
-
-    for i in range(len(players1)):
-        pred[(players1[i],players2[i])] = match_result[i]
-
-    for i in range(nb_iter):
-        tab = [1,2,3,4]
-        random.shuffle(tab)
-        #tab = generateRandomGrid()
-        results[gagner(tab)-1] += 1
-    
-    results /= results.sum()
+    predicator = TournamentPredictor()
+    results = predicator.predict()
     print(results)
     print( np.argmax(results) + 1)
     
