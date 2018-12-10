9f7d3b7e85e5f3d22a01754f877e3eda3bc3fec0
arcticdreamz <tomcrasset@gmail.com>
2018-12-10 16:12:39 +0100

    Drop match features that won't be accessible before the tournament

--- Data_cleaning/joincsv.py
+++ Data_cleaning/joincsv.py
@@ -5,16 +5,39 @@ players = pd.read_csv('cleaned_players_data.csv', dtype=object, encoding='utf-8'
 
 #Rename index as ID
 players.rename(columns={players.columns[0]:"ID"}, inplace=True)
-#Drop index column
+
+#Drop index column an match features, as we will not have them
+#when predicting the winner
+#Drop non numeric columns
+to_drop_matches = ['Games won by winner in set 1' ,
+                  'Games won by loser in set 1' ,
+                  'Games won by winner in set 2' ,
+                  'Games won by loser in set 2' ,
+                  'Games won by winner in set 3' ,
+                  'Games won by loser in set 3' ,
+                  'Games won by winner in set 4' ,
+                  'Games won by loser in set 4' ,
+                  'Games won by winner in set 5' ,
+                  'Games won by loser in set 5' ,
+                  'Winner sets',
+                  'Loser sets',
+                  'Tournament',
+                  'Completed or retired'
+                  ]
+to_drop_players = ['Active','Retired']
+
+players.drop(columns=to_drop_players, inplace=True)
+matches.drop(columns=to_drop_matches, inplace=True)
 matches.drop(matches.columns[0], axis=1, inplace=True)
+
+#Reclean invalid values that are not numeric in a given column
 matches.replace(to_replace='NR',value=0,inplace=True)
 matches.replace(to_replace=' ',value=0,inplace=True)
 matches.replace(to_replace='`1',value=0,inplace=True)
 players.replace(to_replace='Infinity',value=100,inplace=True)
 
 # Split the match time
-players["Match Time"] = players["Match Time"].astype(str).str.split(":", n = 2, expand = True).apply(lambda x: int(x[0]) * 60 + int(x[1]))
-
+players["Match Time"] = players["Match Time"].astype(str).str.split(":").apply(lambda x: int(x[0]) * 60 + int(x[1]))
 players.rename(columns={"Match Time":"Match Time Average"}, inplace=True)
 
 # Remove percentages
@@ -26,19 +49,7 @@ players['After Winning 1st Set'] = players['After Winning 1st Set'].str.rstrip('
 to_rename={'Winner':'PlayerA',
             'Loser'	:'PlayerB',
             'Winner ranking' :'PlayerA ranking',
-            'Loser ranking':'PlayerB ranking',
-            'Games won by winner in set 1':'Games won by PlayerA in set 1',
-            'Games won by loser in set 1':'Games won by PlayerB in set 1',
-            'Games won by winner in set 2':'Games won by PlayerA in set 2',
-            'Games won by loser in set 2':'Games won by PlayerB in set 2',
-            'Games won by winner in set 3':'Games won by PlayerA in set 3',
-            'Games won by loser in set 3':'Games won by PlayerB in set 3',
-            'Games won by winner in set 4':'Games won by PlayerA in set 4',
-            'Games won by loser in set 4':'Games won by PlayerB in set 4',
-            'Games won by winner in set 5':'Games won by PlayerA in set 5',
-            'Games won by loser in set 5':'Games won by PlayerB in set 5',
-            'Winner sets':'PlayerA sets',
-            'Loser sets':'PlayerB sets'
+            'Loser ranking':'PlayerB ranking'
             }
 matches.rename(columns=to_rename, inplace=True)
 
@@ -47,36 +58,13 @@ matches["PlayerA Win"] = '1'
 
 #Randomly swap playerA and playerB values
 #so that the classification doesn't depend on the feature Winner only
-to_swapA = ['PlayerA',
-            'PlayerA ranking',
-            'Games won by PlayerA in set 1',
-            'Games won by PlayerA in set 2',
-            'Games won by PlayerA in set 3',
-            'Games won by PlayerA in set 4',
-            'Games won by PlayerA in set 5',
-            'PlayerA sets'
-             ]
-to_swapB = ['PlayerB',
-            'PlayerB ranking',
-            'Games won by PlayerB in set 1',
-            'Games won by PlayerB in set 2',
-            'Games won by PlayerB in set 3',
-            'Games won by PlayerB in set 4',
-            'Games won by PlayerB in set 5',
-            'PlayerB sets'
-             ]
+to_swapA = ['PlayerA', 'PlayerA ranking']
+to_swapB = ['PlayerB','PlayerB ranking']
 index = matches.sample(frac=0.5,random_state=42).index
 tmp = matches.loc[index,to_swapA].values
 matches.loc[index,to_swapA] = matches.loc[index,to_swapB].values
 matches.loc[index,to_swapB] = tmp
 matches.loc[index,'PlayerA Win'] = 0
-
-
-#Drop unnecessary non-numeric columns
-drop_players = ['Active','Retired']
-drop_matches = ['Tournament','Completed or retired']
-players.drop(columns=drop_players, inplace=True)
-matches.drop(columns=drop_matches, inplace=True)
 
 
 #One hot encoding because Decision tree work with valuesnot strings
@@ -96,9 +84,11 @@ matches = pd.get_dummies(matches, columns = hot_encode_matches)
 players_and_matches = pd.merge(matches,players,left_on="PlayerA",right_on="Name",suffixes=['_Match','_PlayerA'])
 players_and_matches = pd.merge(players_and_matches,players,left_on="PlayerB",right_on="Name",suffixes=['_PlayerA','_PlayerB'])
 
+
 #Drop columns from merged
 todrop_merged = ['Name_PlayerA', 'Name_PlayerB', 'PlayerA','PlayerB']
 players_and_matches.drop(columns=todrop_merged,inplace=True)
+
 
 #Move player columns to the front
 cols = list(players_and_matches.columns.values)
@@ -106,6 +96,7 @@ cols.pop(cols.index('ID_PlayerA'))
 cols.pop(cols.index('ID_PlayerB')) 
 cols.pop(cols.index('PlayerA Win')) 
 players_and_matches = players_and_matches[['ID_PlayerA', 'ID_PlayerB'] + cols + ['PlayerA Win']] #Create new dataframe with columns in the order you want
+
 
 #Convert ID's to numeric values and sort them
 players_and_matches = players_and_matches.apply(pd.to_numeric)
