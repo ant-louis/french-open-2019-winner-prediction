cb9b820bf89d47fa6035351627a5442001b8500f
tcrasset <tomcrasset@gmail.com>
2019-02-21 12:23:03 +0100

    Add user friendly command line arguments

--- Predict_tournament/predict_seeds.py
+++ Predict_tournament/predict_seeds.py
@@ -2,18 +2,58 @@ import pandas as pd
 import itertools
 import os
 import sys
+import argparse
 
 from sklearn.externals import joblib
 from sklearn.ensemble import RandomForestClassifier
 
 PREFIX = '/home/tom/Documents/Uliege/Big-Data-Project'
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        "Tennis Prediction - Create seed matchups and compute win probabilities")
+    parser.add_argument(
+        "--createFile",
+        type=str,
+        default='n',
+        nargs='?',
+        const=True,
+        help="Create new seeds file before preditcting - y/n")
+    parser.add_argument(
+        "--inputFile",
+        type=str,
+        nargs='?',
+        const=True,
+        help="Input file. If createFile is 'y', (seeds_20XX.csv, to_predict20XX.csv) is an example for  \
+                an input and output file pair. \n \
+                Else, the pair  (to_predict20XX.csv, matches_20XX.csv) is recommended.")
+    parser.add_argument(
+        "--outputFile",
+        type=str,
+        default='matches_prediction.csv',
+        nargs='?',
+        const=True,
+        help="Output file. If createFile is 'y', (seeds_20XX.csv, to_predict20XX.csv) is an example for  \
+                an input and output file pair. \n \
+                Else, the pair  (to_predict20XX.csv, matches_20XX.csv) is recommended.")
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="RandomForest_opt.pkl",
+        nargs='?',
+        const=True,
+        help="Name of '.pkl' ML model")
+
+    arguments, _ = parser.parse_known_args()
+    return arguments
+
 
 def createToPredictFile(seed_file, output_file):
 
-
     """Create to_predict csv file from seeds"""
-    matches = pd.read_csv(os.path.join(PREFIX, 'Data_cleaning/training_matches.csv'), dtype=object, encoding='utf-8')
+    
+    print("Creating prediction data from  {} and saving to {}...".format(seed_file, output_file))
+
     players = pd.read_csv(os.path.join(PREFIX, 'Data_cleaning/training_players.csv'), dtype=object, encoding='utf-8')
     players.drop(players.columns[0], axis=1, inplace=True)
 
@@ -56,29 +96,29 @@ def createToPredictFile(seed_file, output_file):
     to_predict['PlayerB ranking'] = to_predict['ID_PlayerB'].values
 
 
-
     # Convert ID's to numeric values and sort them
     to_predict = to_predict.apply(pd.to_numeric)
     to_predict.sort_values(by = ['ID_PlayerA','ID_PlayerB'],inplace=True)
 
     # Export to csv
-    to_predict.to_csv(os.path.join(PREFIX, 'Predict_tournament/Test_data/', output_file),index=False)
+    to_predict.to_csv(os.path.join(PREFIX, 'Predict_tournament', output_file),index=False)
 
 
 #------------------------------------------------------Predicting--------------------------------------------
 
-def predictSeeds(input_file, output_file):
-    to_predict = pd.read_csv(os.path.join(PREFIX, 'Predict_tournament/Test_data/', input_file), dtype=float, encoding='utf-8')
+def predictSeeds(input_file, output_file, modelName):
+    to_predict = pd.read_csv(os.path.join(PREFIX, 'Predict_tournament/', input_file), dtype=float, encoding='utf-8')
     
     to_predict_without_id = to_predict.drop(columns=['ID_PlayerA', 'ID_PlayerB'],inplace=False)
     y_pred_proba = None
 
     #Import estimator
-    estimator_file = "RandomForest_opt.pkl"
+    estimator_file = modelName
     path = os.path.join(PREFIX,'Predict_tournament/', estimator_file)
 
     # Predicting the output, extracting probabilities
     if(os.path.isfile(path)):
+        print("Predicting from {}... using the model {}".format(input_file, modelName))
         model = joblib.load(path)
         y_pred_proba = model.predict_proba(to_predict_without_id)
     else:
@@ -86,6 +126,7 @@ def predictSeeds(input_file, output_file):
         exit()
 
     # Creating the prediction result
+    print("Assembling prediction and saving to {}...".format(output_file))
     prediction = pd.DataFrame()
     prediction['ID_PlayerA'] = to_predict['ID_PlayerA']
     prediction['ID_PlayerB'] = to_predict['ID_PlayerB']
@@ -98,24 +139,39 @@ def predictSeeds(input_file, output_file):
     #     elif y == 1:
     #         prediction.iloc[i,2] = to_predict.iloc[i, 0]
 
-    prediction.to_csv(os.path.join(PREFIX, os.path.join('Predict_tournament/Test_data/', output_file)),index=False)
+    prediction.to_csv(os.path.join(PREFIX, os.path.join('Predict_tournament/', output_file)),index=False)
+    print("Success !")
 
 
 if __name__=='__main__':
 
-    if(len(sys.argv) != 4 ):
-        print("Call with \"python predict_seeds.py predict seeds_20XX.csv to_predict20XX.csv\"")
-        print(" or \"python predict_seeds.py createFile to_predict20XX.csv matches_20XX.csv\"")
-        exit()
+    args = parse_arguments()
     
-    if(sys.argv[1] == 'predict'):
-        predictSeeds(sys.argv[2], sys.argv[3])
-    elif(sys.argv[1] == 'createFile'):
-        createToPredictFile(sys.argv[2], sys.argv[3])
-
+    create_file = args.createFile
+    input_file = args.inputFile
+    output_file = args.outputFile
+    model_name = args.model
+    
+    if create_file == 'y':
+        createToPredictFile(input_file, output_file)
     else:
-        print("Call with \"python predict_seeds.py predict seeds_20XX.csv to_predict20XX.csv\"")
-        print(" or \"python predict_seeds.py createFile to_predict20XX.csv matches_20XX.csv\"")
-        exit()
+        predictSeeds(input_file, output_file, model_name)
+
+
+
+    # if(len(sys.argv) != 4 ):
+    #     print("Call with \"python predict_seeds.py predict seeds_20XX.csv to_predict20XX.csv\"")
+    #     print(" or \"python predict_seeds.py createFile to_predict20XX.csv matches_20XX.csv\"")
+    #     exit()
+    
+    # if(sys.argv[1] == 'predict'):
+    #     predictSeeds(sys.argv[2], sys.argv[3])
+    # elif(sys.argv[1] == 'createFile'):
+    #     createToPredictFile(sys.argv[2], sys.argv[3])
+
+    # else:
+    #     print("Call with \"python predict_seeds.py predict seeds_20XX.csv to_predict20XX.csv\"")
+    #     print(" or \"python predict_seeds.py createFile to_predict20XX.csv matches_20XX.csv\"")
+    #     exit()
     
     
